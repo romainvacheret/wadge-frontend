fc52b928d2e1015767364463f1178d944cb65237
Trystan4 <trystan.rcs4@gmail.com>
2020-10-19 18:10:08 +0200

    Issue_11 Month filter

--- src/components/SeasonList/SeasonList.js
+++ src/components/SeasonList/SeasonList.js
@@ -19,3 +19,32 @@ const SeasonList = () => {
 }
 
 export default SeasonList;
+
+
+
+const [value, setValue] = useState("Janvier");
+const onClick = (value_) => { setValue(value_); };
+function FilterMonth(props){
+    return (
+        <>
+            <label>
+                Choisissez un Mois:
+                <select value={} onChange={ () => onClick(value) }>
+                    <option value="Janvier">Janvier</option>
+                    <option value="Fevrier">Fevrier</option>
+                    <option value="Mars">Mars</option>
+                    <option value="Avril">Avril</option>
+                    <option valeur="Mai">Mai</option>
+                    <option valeur="Juin">Juin</option>
+                    <option valeur="Juillet">Juillet</option>
+                    <option valeur="Aout">Aout</option>
+                    <option valeur="Septembre">Septembre</option>
+                    <option valeur="Octobre">Octobre</option>
+                    <option valeur="Novembre">Novembre</option>
+                    <option valeur="Decembre">Decembre</option>
+                </select>
+            </label>
+            <input type="submit" value="Submit" />
+        </>
+    );
+}
