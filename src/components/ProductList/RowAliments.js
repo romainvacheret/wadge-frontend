1f24533246998c2024c61c4c871c23b28286c58e
sowJamng <maodosow96@gmail.com>
2020-11-06 10:16:40 +0000

    rencensement des fruits et legumes

--- src/components/ProductList/RowAliments.js
+++ src/components/ProductList/RowAliments.js
@@ -1,16 +1,16 @@
 
-import React, { useState,  Component } from 'react';
+import React, {  Component } from 'react';
 
-const foods=[];
-[checked,setChecked]=useState(false)
-const handleCardClick=(food)=> {
-    foods.push(food,food)
-    console.log( food,'clicked')
+// const foods=[];
+
+const handleCardClick=()=> {
+    
+    console.log( 'clicked')
   }
-const myfoods=shuffle(foods);
+// const myfoods=shuffle(foods);
 const Rowalt = ({food})=><tr><td>{food.nom}</td><td>{food.type}</td>
 <td><button onClick={handleCardClick}class="btn btn-info" >
-<input type="checkbox" value="{checked}" onChange="{handleChecked}"/></button></td></tr>
+<input type="checkbox"/></button></td></tr>
 // {checked? myfood.push({food}):null}
 
 
