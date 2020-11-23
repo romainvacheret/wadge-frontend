07ec8da5e7e314822d2f5bd6346dc96647bd693e
Trystan4 <trystan.rcs4@gmail.com>
2020-11-23 19:11:51 +0100

    issue_34 first checkbox

--- src/components/FoodsFridge/DisplayFridge.js
+++ src/components/FoodsFridge/DisplayFridge.js
@@ -1,47 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './DisplayFridge.css';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const DisplayFridge = () => {
+    const [checked, setChecked] = useState(true);
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
     const [fridgeList, setFridgeList] = useState([]);
+        useEffect(() => fetch("http://localhost:8080/display-fridge", {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            })
+                .catch(err => console.log('Error: ', err))
+                .then(response => response.json())
+                .then(response => setFridgeList([...response])),
+            []);
 
-    useEffect(() => fetch("http://localhost:8080/display-fridge", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .catch(err => console.log('Error: ', err))
-            .then(response => response.json())
-            .then(response => setFridgeList([...response])),
-        []);
+        return (
+            <>
+                <img className='Image' src="logo.png" style={{width: 225, height: 100}}/>
+                <h3 className='Title'> Frigo de l'utilisateur </h3>
+                    <ul>
+                        <div>
+                        <Checkbox
+                            checked={checked}
+                            onChange={handleChange}
+                            inputProps={{'aria-label': 'primary checkbox'}}
+                        />
+                        {
+                        fridgeList.map(({nom, produits}, idx) => {
+                                return (
+                                    <div className="List" key={idx}> {
+                                        <>
+                                            <li>{nom}</li>
+                                            <ul> {produits.map(({quantite, dateAjout, dateLimite}) => (
+                                                <>
+                                                    <li>{`Quantite : ${quantite}`}</li>
+                                                    <li>{`Date d'ajout: ${dateAjout}`}</li>
+                                                    <li>{`Date limite: ${dateLimite}`}</li>
+                                                </>
+                                            ))}
 
-    return (
-        <>
-            <img className='Image' src="logo.png" style={{ width: 225, height: 100 }}/>
-            <h3 className='Title'> Frigo de l'utilisateur </h3>
-            <ul> {
-                fridgeList.map(({nom,produits}, idx) => {console.log(produits); return(
-                        <div className="List"key={idx}> {
-                            <>
-                                <li>{ nom }</li>
-                                <ul> {produits.map(({ quantite, dateAjout, dateLimite}) => (
-                                    <>
-                                    <li>{ `Quantite : ${quantite}` }</li>
-                                    <li>{ `Date d'ajout: ${dateAjout}` }</li>
-                                    <li>{ `Date limite: ${dateLimite}` }</li>
-                                    </>
-                                    ) )}
-
-                                </ul>
-                                <br></br>
-                            </>
-                        } </div>
-                    );}
-                )
-            } </ul>
-        </>
-    );
+                                            </ul>
+                                            <br></br>
+                                        </>
+                                    } </div>
+                                );
+                            }
+                        )
+                    }
+                        </div>
+                    </ul>
+            </>
+        );
 }
-
 export default DisplayFridge;
