44589ce23b6d0e66caffb55c1c6251fc0ec7f5cc
FannyLourioux <f.lourioux@gmail.com>
2020-12-07 14:13:02 +0100

    fix: update foodCard display

--- src/components/food/FoodRepresentation/FoodRepresentation.js
+++ src/components/food/FoodRepresentation/FoodRepresentation.js
@@ -19,21 +19,37 @@ const useStyles = makeStyles({
         strokeWidth: 2,
     },
     word: {
-        fontSize: '13px',
-        strokeWidth: 1
+        fontSize: '10px',
+        strokeWidth: 0.5
     }
 });
 
-const FoodRepresentation = ({ word, color}) => {   
-    const classes = useStyles(); 
+const FoodRepresentation = ({ word, color}) => {
+    const classes = useStyles();
     word = word.charAt(0).toUpperCase() + word.slice(1);
     const letter = word.slice(0, 2);
+    const wordTab = word.split(' ');
+    let endWord = '';
+    let flag = true;
+    if (wordTab.length){
+        flag = false;
+        endWord = wordTab[wordTab.length -1] ;
+        word = word.replace(endWord,'');
+    }
 
     return (
         <svg x='0' y='0' className={ classes.svg }>
             <rect x='0' y='0' className={ classes.rect } fill={ color }/>
-            <text x='7' y='45' className={ classes.letter }> { letter }</text>
-            <text x='5' y='63' className={ classes.word }>{ word }</text>
+            <text x='7' y='38' className={ classes.letter }> { letter }</text>
+
+            { flag ? (
+                <text x='4' y='55' className={ classes.word }>{ word }</text>
+            ): (
+                <>
+                    <text x='4' y='53' className={ classes.word }>{ word }</text>
+                    <text x='4' y='63' className={ classes.word }>{ endWord }</text>
+                </>
+            ) }
         </svg>
     );
 }
