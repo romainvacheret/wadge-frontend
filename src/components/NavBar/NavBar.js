10b23706ee19217283dbe0aadb6eac06defa5f35
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-15 08:31:27 +0100

    fix: correct css classes and ids

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,22 +9,22 @@ const NavBar = (props) => {
     return (
         <AppBar position="static" color="transparent" id="navbar">
           <Toolbar  >
-            <ListItemLink href="/" id="nav-logo">
+            <ListItemLink href="/" className='menu' id="nav-logo">
               <img src="logo.png" id="logo-nav" width="180" height="80"/>
             </ListItemLink>
-            <ListItemLink href="/filter" id="nav-filter">
+            <ListItemLink href="/filter" className='menu' id="nav-filter">
               Fruits et Légumes
             </ListItemLink>
-            <ListItemLink href="/fridge" id="nav-fridge">
+            <ListItemLink href="/fridge" className='menu' id="nav-fridge">
               Frigo
             </ListItemLink>
-            <ListItemLink href="/fridge_addition" id="nav-fridge-addition">
+            <ListItemLink href="/fridge_addition" className='menu' id="nav-fridge-addition">
               Ajout au frigo
             </ListItemLink>
-            <ListItemLink href="/map" id="nav-map">
+            <ListItemLink href="/map" className='menu' id="nav-map">
               Carte des Magasins
             </ListItemLink>
-            <ListItemLink href="/recipes" id="nav-recipes">
+            <ListItemLink href="/recipes" className='menu' id="nav-recipes">
               Recettes
             </ListItemLink>
           </Toolbar>
