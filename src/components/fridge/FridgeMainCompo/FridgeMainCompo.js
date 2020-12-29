5579c841a86758e489518807995721cbfb383c7b
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-29 11:07:40 +0100

    style: order fridge categories

--- src/components/fridge/FridgeMainCompo/FridgeMainCompo.js
+++ src/components/fridge/FridgeMainCompo/FridgeMainCompo.js
@@ -2,59 +2,31 @@ import React, { useState, useEffect } from 'react';
 import {Typography, Grid, Container} from '@material-ui/core';
 import './FridgeMainCompo.css';
 
-import FoodCard from '../../food/FoodCard/FoodCard';
-import colorList from '../../food/FoodList/color_list.json';
+import FridgeCategory from './FridgeCategory';
 
 import { fetchFromUrl } from 'utils'; 
 
 const DisplayFridge = () => {
     const [fridgeList, setFridgeList] = useState({});
 
-    const textFromKey = {
-        'TWO_DAYS': 'deux jours',
-        'FIVE_DAYS': 'cinq jours',
-        'SEVEN_DAYS': 'sept jours',
-        'FORTEEN_DAYS': 'quatorze jours',
-        'EXPIRED': 'Produits périmés',
-        'OTHER': 'plus de quatorze jours'
-    };
-
     useEffect(() => {
         fetchFromUrl('alerts', setFridgeList);
     }, []);
 
+    console.log('fridge', fridgeList);
     return (
         <>
-            <Typography variant='h2' className='fridge-main-compo__label' > Frigo de l'utilisateur </Typography>
+            <Typography variant='h2' className='fridge-main-compo__label' > Contenu de votre frigo </Typography>
             <Container className='fridge-main-compo__container'>
                 <Grid >{
-                    Object.keys(fridgeList).map((key, idx) => {
-                        return fridgeList[key].length ? (
-                            <div key={ idx }>
-                                <Typography className='fridge-main-compo__label' variant='h4'>{ key === 'EXPIRED' ? textFromKey[key] : `Produits à manger au plus tard dans ${textFromKey[key]}`}</Typography>
-                                <Grid container justify='center'
-                                    alignItems='center'>
-                                    {
-                                        fridgeList[key].map(food => {
-                                            const { name, products } = food;
-                                            const restructuredFood = products.map(product => { return {name, ...product} })
-                                            return restructuredFood.flat();
-                                        }).map((food_, idx_) => {
-                                            const { name, insertionDate, quantity } = food_[0];
-                                            return (
-                                            <Grid
-                                                key={ idx_ }
-                                                item
-                                            >
-                                                <FoodCard foodAttributes={{ word: name, color: colorList[name] }} data={{ "Date d'ajout": insertionDate, "Quantité": quantity }}/>
-
-                                            </Grid>); 
-                                        })
-                                    }
-                                </Grid>
-                            </div>
-                        ) : '';
-                    })
+                    Object.keys(fridgeList).length ?( <>
+                        <FridgeCategory fridgeList={ fridgeList['TWO_DAYS'] } text={ 'Préremption dans 2 jours' }/>
+                        <FridgeCategory fridgeList={ fridgeList['FIVE_DAYS'] } text={ 'Préremption dans 5 jours' }/>
+                        <FridgeCategory fridgeList={ fridgeList['SEVEN_DAYS'] } text={ 'Préremption dans 7 jours' }/>
+                        <FridgeCategory fridgeList={ fridgeList['FORTEEN_DAYS'] } text={ 'Préremption dans 14 jours' }/>
+                        <FridgeCategory fridgeList={ fridgeList['OTHER'] } text={ 'Préremption dans plus de 14 jours' }/>
+                        <FridgeCategory fridgeList={ fridgeList['EXPIRED'] } text={ 'Périmés' }/>
+                    </> ): <></>
                 }</Grid>
             </Container>
         </>
