408188fce740728cc566046e41491785309acf5f
FannyLourioux <f.lourioux@gmail.com>
2020-12-30 21:56:32 +0100

    style: recipes dislpay

--- src/components/recipe/RecipeList/RecipeList.js
+++ src/components/recipe/RecipeList/RecipeList.js
@@ -2,7 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import './RecipeList.css';
 
-import { fetchFromUrl } from 'utils'; 
+import { fetchFromUrl } from 'utils';
+import {Container, Typography} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+
 
 const RecipeList = () => {
     const [recipeList, setRecipeList] = useState([]);
@@ -13,37 +22,47 @@ const RecipeList = () => {
 
     return (
         <>
-            <h3 className='recipe-liste__title'> Liste des recettes </h3>
-            <ul> {
+            <Typography variant="h3" className='recipe-liste__title'> Liste des recettes </Typography>
+            <div> {
             recipeList.map(({ steps, ingredients, name, servings, difficulty}, idx) => 
                 <div key={ idx }> { 
                     <>
-                        <ul>
-                            <li>{ name }</li>
-                            <li>{ `Recette pour : ${ servings } personnes` }</li>
-                            <li>{ `Difficulte : ${ difficulty }/4` }</li>
-                            <li>
-                                <ul>
-                                    <h4>Etapes</h4>
-                                    {steps.map((step, idx_) => <li key={ idx_ }> { step } </li>)}
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <h4>Ingredients</h4>
-                                    { ingredients.map((ingredient, idx__) =>{
-                                        let quantity = ingredient.quantity;
-                                        return (<li key={ idx__ }> {ingredient.name} { quantity !== -1 ? (' : ' + ingredient.quantity) : ''} </li>)
-                                    })}
-                                    
-                                </ul>
-                            </li>
-                        </ul>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon fontSize="large"/>}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography variant="h4" className="recipe_name">{name}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Grid>
+                                    <Typography variant="h5">{ `Recette pour : ${ servings } personnes` }</Typography>
+                                    <Typography variant="h5">{ `Difficulté : ${ difficulty }/4` }</Typography>
+                                </Grid>
+                                <Grid>
+                                        <ul>
+                                            <h4 className="step-recipe">Etapes</h4>
+                                            {steps.map((step, idx_) => <Grid key={ idx_ }> { step } </Grid>)}
+                                        </ul>
+                                    </Grid>
+                                    <Grid>
+                                        <ul>
+                                            <h4 className="step-recipe">Ingredients</h4>
+                                            { ingredients.map((ingredient, idx__) =>{
+                                                let quantity = ingredient.quantity;
+                                                return (<Grid key={ idx__ }> {ingredient.name} { quantity !== -1 ? (' : ' + ingredient.quantity) : ''} </Grid>)
+                                            })}
+
+                                        </ul>
+                                    </Grid>
+                            </AccordionDetails>
+                        </Accordion>
                         <br></br>
                     </>
                 } </div> 
                 )
-            } </ul>
+            } </div>
         </>
     );
 }
