d8f516d64553f5222f0a4f16bb8796ee824c88a3
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-31 10:29:30 +0100

    refactor: create new component for recipes' card

--- src/components/recipe/RecipeList/RecipeList.js
+++ src/components/recipe/RecipeList/RecipeList.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 import './RecipeList.css';
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 import { fetchFromUrl } from 'utils';
-import { Grid, Typography, Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
+import { Typography } from "@material-ui/core";
 
 const RecipeList = () => {
     const [recipeList, setRecipeList] = useState([]);
@@ -17,44 +16,9 @@ const RecipeList = () => {
     return (
         <>
             <Typography variant="h3" className='recipe__title'> Liste des recettes </Typography>
-            <div> {
-            recipeList.map(({ steps, ingredients, name, servings, difficulty}, idx) => 
-                <div key={ idx }> { 
-                    <>
-                        <Accordion>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon fontSize="large"/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h4" className="recipe__name">{ name }</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Grid>
-                                    <Typography variant="h5">{ `Recette pour : ${ servings } personnes` }</Typography>
-                                    <Typography variant="h5">{ `Difficulté : ${ difficulty }/4` }</Typography>
-                                </Grid>
-                                <Grid>
-                                        <ul>
-                                            <Typography variant="h4" className="step__recipe">Etapes</Typography>
-                                            {steps.map((step, idx_) => <Grid key={ idx_ }> { step } </Grid>)}
-                                        </ul>
-                                    </Grid>
-                                    <Grid>
-                                        <ul>
-                                            <Typography variant="h4" className="step__recipe">Ingredients</Typography>
-                                            { ingredients.map((ingredient, idx__) => 
-                                                <Grid key={ idx__ }> 
-                                                    {ingredient.name} { ingredient.quantity !== '-1' ? (' : ' + ingredient.quantity) : ''}
-                                                </Grid>
-                                            )}
-                                        </ul>
-                                    </Grid>
-                            </AccordionDetails>
-                        </Accordion>
-                        <br></br>
-                    </>
-                } </div> 
+            <div className='recipe__container'> {
+            recipeList.map((recipe, idx) => 
+                <RecipeCard recipe={ recipe } key={ idx }/>
                 )
             } </div>
         </>
