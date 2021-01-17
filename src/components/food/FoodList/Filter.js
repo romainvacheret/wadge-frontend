8a9799513633ae84c3214de64a24ad968ceb0fd5
sowJamng <maodosow96@gmail.com>
2021-01-17 13:37:24 +0000

    feat:add seachBar food by Name

--- src/components/food/FoodList/Filter.js
+++ src/components/food/FoodList/Filter.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect,useState  } from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import './Filter.css';
-
+const months = ["january", "february", "march", "april", "may", "jun", "july", "august", "september", "october", "november", "december"];
 const Filter = ({ tabNumber, setTabNumber, setFoodList }) => {
-    const months = ["january", "february", "march", "april", "may", "jun", "july", "august", "september", "october", "november", "december"];
+    const [choice,setChoice]=useState(0);
 
-    const getProducts = (month) => {
-        fetch(`http://localhost:8080/foods/${month}`, {
+    const getProductsSortDays = (month,choice) => {
+       
+            fetch(`http://localhost:8080/foods/${month}/${choice}`, {
            method: "GET",
             headers: {
                 'Accept': 'application/json',
@@ -17,13 +20,36 @@ const Filter = ({ tabNumber, setTabNumber, setFoodList }) => {
         .then(response => setFoodList([...response]))
         .catch(err => console.log('ERROR', err));
     };
+   
+    
+    useEffect(()=>getProductsSortDays(months[tabNumber],choice),[tabNumber,choice]);
 
-    useEffect(() => getProducts(months[tabNumber]), [tabNumber]);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      
+      };
+    const handleClose = () => {
+        setAnchorEl(null);
+      };
 
     return (
+    
+        
         <div className="monthfilter__bar">
-                <div onClick={ () => setTabNumber(0) }  className={`monthfilter-bar__tab ${tabNumber === 0 ? 'monthfilter-bar__tab__current': ''}`}>Janvier</div>
-                <div onClick={ () => setTabNumber(1) }  className={`monthfilter-bar__tab ${tabNumber === 1 ? 'monthfilter-bar__tab__current': ''}`}>Février</div>
+                <div onClick={handleClick} aria-controls="simple-menu" aria-haspopup="true"  className={`monthfilter-bar__tab ${tabNumber === 0 ? 'monthfilter-bar__tab__current': ''}`}>Janvier
+                
+                <Menu
+                     id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}>
+                    <MenuItem onClick={ () => { setTabNumber(0);setChoice(0);}}>Tier Par Nom</MenuItem>
+                    <MenuItem onClick={ () => {setTabNumber(0);setChoice(1); }}>Trier Par Peremption</MenuItem>
+                    </Menu>
+                    </div>
+                <div onClick={ () => setTabNumber(1) }className={`monthfilter-bar__tab ${tabNumber === 1 ? 'monthfilter-bar__tab__current': ''}`}>Février</div>
                 <div onClick={ () => setTabNumber(2) }     className={`monthfilter-bar__tab ${tabNumber === 2 ? 'monthfilter-bar__tab__current': ''}`}>Mars</div>
                 <div onClick={ () => setTabNumber(3) }    className={`monthfilter-bar__tab ${tabNumber === 3 ? 'monthfilter-bar__tab__current': ''}`}>Avril</div>
                 <div onClick={ () => setTabNumber(4) }      className={`monthfilter-bar__tab ${tabNumber === 4 ? 'monthfilter-bar__tab__current': ''}`}>Mai</div>
@@ -35,6 +61,7 @@ const Filter = ({ tabNumber, setTabNumber, setFoodList }) => {
                 <div onClick={ () => setTabNumber(10)} className={`monthfilter-bar__tab ${tabNumber === 10 ? 'monthfilter-bar__tab__current': ''}`}>Novembre</div>
                 <div onClick={ () => setTabNumber(11)} className={`monthfilter-bar__tab ${tabNumber === 11 ? 'monthfilter-bar__tab__current': ''}`}>Décembre</div>
         </div>
+     
     );
 }
 
--- src/components/food/FoodList/FoodList.js
+++ src/components/food/FoodList/FoodList.js
@@ -8,16 +8,21 @@ import './FoodList.css';
 import FoodCard from "../FoodCard/FoodCard";
 import FruitsAndVegetables from '../FruitsAndVegetables/FruitsAndVegetables';
 import Filter from './Filter';
+import SearchBar from '../../SearchBar/SearchBar';
 
 const FoodList = () => {
     const [foodList, setFoodList] = useState([]);
     const [tabNumber, setTabNumber] = useState(new Date().getMonth());
+    const [searchFood, setSearchFood] = useState('');
 
     const vegetables = [];
     const fruits = [];
 
     const sortFood = () => {
         foodList.forEach(({ name, type, days }, idx) => {
+            if(searchFood.length > 0 && name.indexOf(searchFood) === -1) {
+                return;
+            }
             const element = (
                 <Grid item className='foodlist__food-card' key={ idx } >
                     <FoodCard foodAttributes={{ word: name, color: colorList[name] }} data= {{ 'Conservation': `${days} jours` }}/>
@@ -27,14 +32,21 @@ const FoodList = () => {
         })
     };
     sortFood();
-
+    const handleChange = (event) => setSearchFood(event.target.value);
     return (
         <>
+
             <Typography variant="h3" gutterBottom className="foodlist__title">
                 Liste des fruits et légumes
             </Typography>
+            <>
+            <Grid container item xs={12} spacing={4}>
+            <SearchBar searchFood={ searchFood } handleChange={ handleChange } />
+             </Grid>
+             </>
+            
             <Filter tabNumber = { tabNumber }setTabNumber={ setTabNumber } setFoodList={ setFoodList }/>
-            <FruitsAndVegetables fruits={ fruits } vegetables={ vegetables }/>
+             <FruitsAndVegetables fruits={ fruits } vegetables={ vegetables }/>
         </>
     );}
 
