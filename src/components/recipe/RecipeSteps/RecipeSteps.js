7a2c15ace8d445af86b4207acf0f39458e3fe5f1
FannyLourioux <f.lourioux@gmail.com>
2021-01-28 10:46:39 +0100

    feat: add recipe steps component

---
+++ src/components/recipe/RecipeSteps/RecipeSteps.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+
+import {
+    Button,
+    Grid,
+    Step,
+    StepContent,
+    StepLabel,
+    Stepper,
+    Typography
+} from "@material-ui/core";
+
+const RecipeSteps = ({ recipe }) => {
+    const {recipeSteps, ingredients, name, servings, difficulty} = recipe;
+    const nbStep = recipeSteps.length;
+
+    const getSteps = ()=> {
+        const s = [];
+        for (let j = 0; j < nbStep; j++) {
+            s.push("Etape " + (j + 1));
+        }
+        return s;
+    }
+
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    return (
+
+        <div className="je_sais_pas">
+            <Typography variant="h4" className="recipe__name">{ name }</Typography>
+            <Grid>
+                <Typography variant="h5">{ `Recette pour : ${ servings } personnes` }</Typography>
+                <Typography variant="h5">{ `Difficulté : ${ difficulty }/4` }</Typography>
+            </Grid>
+            <Stepper activeStep={activeStep} orientation="vertical">
+                {steps.map((label, index) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                        <StepContent>
+                            <Typography>{recipeSteps[index]}</Typography>
+                            <div className="faut_tester_pour_savoir">
+                                <div>
+                                    <Button
+                                        disabled={activeStep === 0}
+                                        onClick={handleBack}
+                                        className="button"
+                                    >
+                                        Retour à l'étape précédente
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleNext}
+                                        className="button"
+                                    >
+                                        {activeStep === steps.length - 1 ? 'Terminer la recette' : 'Etape suivante'}
+                                    </Button>
+                                </div>
+                            </div>
+                        </StepContent>
+                    </Step>
+                ))}
+            </Stepper>
+        </div>
+    );
+}
+
+export default RecipeSteps;
