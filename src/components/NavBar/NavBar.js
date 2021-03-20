623021090c4bb30240a20b863f7d7081d0a91e14
Romain Vacheret <romain.vacheret@gmail.com>
2021-03-20 07:11:02 +0100

    test: add tests for navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -29,7 +29,7 @@ const NavBar = () => {
             <ListItemLink href="/fridge/addition" className='navbar__links' id="nav-fridge-addition">
               Ajout au frigo
             </ListItemLink>
-            <ListItemLink href="/shopping_list" className='navbar__links' id="nav-map">
+            <ListItemLink href="/shopping_list" className='navbar__links' id="nav-shopping">
               Liste des courses
             </ListItemLink>
             <ListItemLink href="/recipes" className='navbar__links' id="nav-recipes">
